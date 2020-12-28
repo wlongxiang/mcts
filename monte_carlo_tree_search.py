1ec9d6496d4315774387affbb43cd1181193ac5e
Benjamin Wang <wlongxiang1119@gmail.com>
2020-12-28 21:27:27 +0100

    Some refactoring

--- monte_carlo_tree_search.py
+++ monte_carlo_tree_search.py
@@ -1,8 +1,6 @@
 """
-A minimal implementation of Monte Carlo tree search (MCTS) in Python 3
-Luke Harold Miles, July 2019, Public Domain Dedication
-See also https://en.wikipedia.org/wiki/Monte_Carlo_tree_search
-https://gist.github.com/qpwo/c538c6f73727e254fdc7fab81024f6e1
+BenjaminWang Dec. 2020.
+Adapted from: https://gist.github.com/qpwo/c538c6f73727e254fdc7fab81024f6e1
 """
 from collections import defaultdict
 import math
